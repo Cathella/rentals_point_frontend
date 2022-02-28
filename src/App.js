0b7c082a9b9e9791a326fe34634e0b8c8b384303
Cathella <ncathie214@gmail.com>
2022-02-28 18:20:39 +0300

    Configure app routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Add from './pages/Add';
+import Remove from './pages/Remove';
+import Favorites from './pages/Favorites';
+import Details from './pages/Details';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">Hello</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/remove" element={<Remove />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
